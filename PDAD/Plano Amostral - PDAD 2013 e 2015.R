5d5397714269d5f356956ae47359b187fb15e4c9
thiagomendesrosa <thiago.rosa@codeplan.df.gov.br>
2017-07-31 11:17:12 -0300

    Ok.

--- PDAD/Plano Amostral - PDAD 2013 e 2015.R
+++ PDAD/Plano Amostral - PDAD 2013 e 2015.R
@@ -1,15 +1,20 @@
-rm(list = ls())
-gc()
+# Limpar o ambiente e a memória
+# rm(list = ls())
+# gc()
 
-packages <- c("data.table", "survey")
-if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-  install.packages(setdiff(packages, rownames(installed.packages())))  
-}
+# Listar os pacotes necessários
+list.of.packages <- c("data.table", "survey")
 
+# Instalar os pacotes necessários, caso eles não estejam instalados
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+# Carregar os pacotes
 library(data.table)
 library(survey)
 
-
+# Remover os objetos
+rm(list.of.packages,new.packages)
 
 # Declarar o plano amostral da PDAD 2013 ####
 
@@ -21,7 +26,7 @@ pdad_dom_2013 <- fread("https://raw.githubusercontent.com/codeplanprojetos/util/
 pdad_pes_2013 <- fread("https://raw.githubusercontent.com/codeplanprojetos/util/master/PDAD/2013/PDAD_PES_2013.csv",
                        dec = ",", encoding = "Latin-1")
 
-# Unir informações de pessoas e domicílios
+# Unir informações de pessoas e domicílios, desconsiderando as variáveis comuns
 pdad_2013 <- merge(pdad_pes_2013, 
                    pdad_dom_2013[,-c(2,4:6,110:114)],
                    all.x = T,by="CD_SEQ_DOM")
@@ -39,16 +44,16 @@ sample.pdad2013 <-
     nest=TRUE
   )
 
-# Criar um objeto para pós_estrato
+# Criar um objeto para pós estrato
 post.pop<-unique(subset(pdad_2013,select=c(POPULACAO_AJUSTADA)))
 
 # Criar a variável de frequência (a mesma variável de pós-estrato, para funcionar como id e peso)
 post.pop$Freq <- post.pop$POPULACAO_AJUSTADA
 
-# Declarar o objeto de pós-estrato
+# Declarar o objeto de pós estrato
 svy2013 <- postStratify(sample.pdad2013,~POPULACAO_AJUSTADA,post.pop)
 
-# Ajusta estratos com apenas uma UPA (adjust=centered)
+# Ajustar estratos com apenas uma UPA (adjust=centered)
 options( survey.lonely.psu = "adjust")
 
 # Verificar total da população
@@ -68,7 +73,7 @@ pdad_dom_2015 <- fread("https://raw.githubusercontent.com/codeplanprojetos/util/
 pdad_pes_2015 <- fread("https://raw.githubusercontent.com/codeplanprojetos/util/master/PDAD/2015/PDAD_PES_2015.csv",
                        dec = ",", encoding = "Latin-1")
 
-# Unir informações de pessoas aos parâmetros do plano amostral
+# Unir informações de pessoas e domicílios, desconsiderando as variáveis comuns
 pdad_2015 <- merge(pdad_pes_2015, 
                    pdad_dom_2015[,-c(3:6,102:105)],
                        all.x = T,by="CD_SEQ_DOM")
@@ -86,7 +91,7 @@ sample.pdad2015 <-
     nest=TRUE
   )
 
-# Criar um objeto para pós_estrato
+# Criar um objeto para pós estrato
 post.pop<-unique(subset(pdad_pes_2015,select=c(POPULACAO_AJUSTADA)))
 
 # Criar a variável de frequência (a mesma variável de pós-estrato, para funcionar como id e peso)
@@ -95,7 +100,7 @@ post.pop$Freq <- post.pop$POPULACAO_AJUSTADA
 # Declarar o objeto de pós-estrato
 svy2015 <- postStratify(sample.pdad2015,~POPULACAO_AJUSTADA,post.pop)
 
-# Ajusta estratos com apenas uma UPA (adjust=centered)
+# Ajustar estratos com apenas uma UPA (adjust=centered)
 options( survey.lonely.psu = "adjust")
 
 # Verificar total da população
@@ -104,5 +109,6 @@ svytotal(~count,svy2015) # 2.906.855.
 # Verificar total da população por sexo
 svyby(~count,~D04_MOR_SEXO,svy2015,svytotal) # 1.391.640 homens, 1.515.215  mulheres.
 
+# Remover objetos
 # rm(list = ls())
 # gc()
